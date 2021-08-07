aaacee728f987ca873a7f3fa9ccad610939359fa
Akshay BM <akshaybmnmd@gmail.com>
2021-08-07 14:52:07 +0530

    added image model

--- views/images.php
+++ views/images.php
@@ -43,7 +43,128 @@ $conn->close();
   }
 </script>
 
+<style>
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  #myImg {
+    border-radius: 5px;
+    cursor: pointer;
+    transition: 0.3s;
+  }
+
+  #myImg:hover {
+    opacity: 0.7;
+  }
+
+  /* The Modal (background) */
+  .modal {
+    display: none;
+    /* Hidden by default */
+    position: fixed;
+    /* Stay in place */
+    z-index: 1;
+    /* Sit on top */
+    padding-top: 100px;
+    /* Location of the box */
+    left: 0;
+    top: 0;
+    width: 100%;
+    /* Full width */
+    height: 100%;
+    /* Full height */
+    overflow: auto;
+    /* Enable scroll if needed */
+    background-color: rgb(0, 0, 0);
+    /* Fallback color */
+    background-color: rgba(0, 0, 0, 0.9);
+    /* Black w/ opacity */
+  }
+
+  /* Modal Content (image) */
+  .modal-content {
+    margin: auto;
+    display: block;
+    width: 80%;
+    max-width: 700px;
+  }
+
+  /* Caption of Modal Image */
+  #caption {
+    margin: auto;
+    display: block;
+    width: 80%;
+    max-width: 700px;
+    text-align: center;
+    color: #ccc;
+    padding: 10px 0;
+    height: 150px;
+  }
+
+  /* Add Animation */
+  .modal-content,
+  #caption {
+    -webkit-animation-name: zoom;
+    -webkit-animation-duration: 0.6s;
+    animation-name: zoom;
+    animation-duration: 0.6s;
+  }
+
+  @-webkit-keyframes zoom {
+    from {
+      -webkit-transform: scale(0)
+    }
+
+    to {
+      -webkit-transform: scale(1)
+    }
+  }
+
+  @keyframes zoom {
+    from {
+      transform: scale(0)
+    }
+
+    to {
+      transform: scale(1)
+    }
+  }
+
+  /* The Close Button */
+  .close {
+    position: absolute;
+    top: 15px;
+    right: 35px;
+    color: #f1f1f1;
+    font-size: 40px;
+    font-weight: bold;
+    transition: 0.3s;
+  }
+
+  .close:hover,
+  .close:focus {
+    color: #bbb;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  /* 100% Image Width on Smaller Screens */
+  @media only screen and (max-width: 700px) {
+    .modal-content {
+      width: 100%;
+    }
+  }
+</style>
+
 <div class="container">
+
+  <!-- The Modal -->
+  <div id="myModal" class="modal">
+    <span class="close">&times;</span>
+    <img class="modal-content" id="img01">
+    <div id="caption"></div>
+  </div>
 
   <?php
   $ratio = 0.3;
@@ -51,8 +172,9 @@ $conn->close();
 
   foreach ($images as $img) {
     $output .= "<div class=\"photo\">";
-    $output .= "<a target=\"_blank\" href=\"{$img['path']}\">";
-    $output .= "<img class=\"lazy\" data-original=\"{$img['path']}\" alt=\"\"></a><br>\n";
+    // $output .= "<a target=\"_blank\" href=\"{$img['path']}\">";
+    // $output .= "<img class=\"lazy\" data-original=\"{$img['path']}\" alt=\"\"></a><br>\n";
+    $output .= "<img class=\"lazy\" src=\"{$img['path']}\" data-original=\"{$img['path']}\" alt=\"\"><br>\n";
     $output .= "({$img['mem']} KB) <button id='{$img['id']}' onclick='change_privacy({$img['id']})'>Make " . ($img['privacy'] == 'public' ? "private" : "public") . "</button><br>";
     $output .= "{$img['like']} likes<br>\n";
     $output .= "</div>\n";
@@ -66,9 +188,33 @@ $conn->close();
 </div>
 
 <script type="text/javascript" charset="utf-8">
-  $(function() {
-    $("img.lazy").lazyload();
+  var images = <?php echo json_encode($images); ?>;
+
+  var modal = document.getElementById("myModal");
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  // var img = document.getElementsByClassName("lazy");
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+
+  $('img').click((t) => {
+    console.log(t.target);
+    modal.style.display = "block";
+    modalImg.src = t.target.src;
+    captionText.innerHTML = t.target.alt;
   });
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+
+    $(function() {
+      $("img.lazy").lazyload();
+    });
+  }
 
   function change_privacy(id) {
     $.post("actions/edit_privacy.php", {

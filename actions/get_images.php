751039bc7689a55d6f2c65da00df7b41599ca3c7
Akshay BM <akshaybmnmd@gmail.com>
2021-08-07 19:01:53 +0530

    added image get api

---
+++ actions/get_images.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+header('Content-Type: application/json');
+$response['server'] = "ok";
+require "../common/db.php";
+$response['DB'] = "ok";
+
+$user_id = $_SESSION['user_id'];
+$response['user'] = $user_id;
+
+if (@$_POST['token'] == "sdlhg3567$#^fhgE%yuY?34") {
+    $user_id = $_POST['user_id'];
+}
+
+$response['user_id'] = $user_id;
+
+$sql = "SELECT * FROM `images` WHERE `user_id` = '$user_id' AND `location` = 'local_hdd_U_i' ORDER BY `time` DESC LIMIT 0,10";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        $id = $row["id"];
+        $images[] = array(
+            'id' => $id,
+            'name' => $row['Name'],
+            'path' => $row["path"],
+            'mem' => round($row["size"] / 1024),
+            'like' => $row["likes"],
+            'privacy' => $row['privacy']
+        );
+    }
+} else {
+    $images = [];
+}
+$conn->close();
+$response['data'] = $images;
+echo json_encode($response);
